60827e86d330dbcdf0576afb954e823ac4e68028
PatchalaMahitha <patchalanagamahitha@gmail.com>
2026-07-17 15:15:23 +0530

    Added Wipro Java hands-on assignments

---
+++ Wipro_TNP/src/threadcontrol/EvenOddJoin.java
@@ -0,0 +1,50 @@
+package threadcontrol;
+
+class EvenThread extends Thread {
+
+    @Override
+    public void run() {
+
+        System.out.println("Even Numbers:");
+
+        for (int i = 2; i <= 20; i += 2) {
+            System.out.print(i + " ");
+        }
+
+        System.out.println();
+    }
+}
+
+class OddThread extends Thread {
+
+    @Override
+    public void run() {
+
+        System.out.println("Odd Numbers:");
+
+        for (int i = 1; i <= 20; i += 2) {
+            System.out.print(i + " ");
+        }
+
+        System.out.println();
+    }
+}
+
+public class EvenOddJoin {
+
+    public static void main(String[] args) {
+
+        EvenThread even = new EvenThread();
+        OddThread odd = new OddThread();
+
+        even.start();
+
+        try {
+            even.join();     // Wait until even thread finishes
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        odd.start();
+    }
+}
---
+++ Wipro_TNP/src/threadcontrol/ThreadPriorityDemo.java
@@ -0,0 +1,36 @@
+package threadcontrol;
+
+class PriorityThread extends Thread {
+
+    public PriorityThread(String name) {
+        super(name);
+    }
+
+    @Override
+    public void run() {
+
+        for (int i = 1; i <= 5; i++) {
+            System.out.println(getName() +
+                    " Priority: " + getPriority() +
+                    " Count: " + i);
+        }
+    }
+}
+
+public class ThreadPriorityDemo {
+
+    public static void main(String[] args) {
+
+        PriorityThread t1 = new PriorityThread("MAX Thread");
+        PriorityThread t2 = new PriorityThread("MIN Thread");
+        PriorityThread t3 = new PriorityThread("NORM Thread");
+
+        t1.setPriority(Thread.MAX_PRIORITY);     //10
+        t2.setPriority(Thread.MIN_PRIORITY);     //1
+        t3.setPriority(Thread.NORM_PRIORITY);    //5
+
+        t1.start();
+        t2.start();
+        t3.start();
+    }
+}
