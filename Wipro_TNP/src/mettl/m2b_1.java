1805a886378dfc5fef1a41ab94d6a89b0584130f
PatchalaMahitha <patchalanagamahitha@gmail.com>
2026-08-17 22:20:23 +0530

    Added Wipro HandsonAssaignment

---
+++ Wipro_TNP/src/mettl/m2b_1.java
@@ -0,0 +1,30 @@
+package mettl;
+
+public class m2b_1 {
+
+    public static int AddSub(int input1, int input2) {
+
+        int result = 0;
+
+        if (input2 == 1) {
+            for (int i = input1; i >= 1; i--) {
+                if ((input1 - i) % 2 == 0)
+                    result += i;
+                else
+                    result -= i;
+            }
+        } 
+        else if (input2 == 2) {
+            result = input1;
+
+            for (int i = input1 - 1; i >= 1; i--) {
+                if ((input1 - i) % 2 == 0)
+                    result += i;
+                else
+                    result -= i;
+            }
+        }
+
+        return result;
+    }
+}
